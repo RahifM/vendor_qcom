75000e07998efe69a9f2789b466deab11621c2bc
blong <blong@codeaurora.org>
2018-04-25 09:06:19 +0800

    msm8953_32: Rename msm_irqbalance_sdm632.conf to msm_irqbalance_little_big.conf
    In order to runtime change msm irqbalance conf file,
    need to use exist vendor.msm_irqbal_lb service which
    is using msm_irqbalance_little_big.conf file.
    
    Change-Id: Iee1b548c698081e2d900ad38253ff1f201fd72dc

--- msm8953_32.mk
+++ msm8953_32.mk
@@ -176,7 +176,7 @@ PRODUCT_PACKAGES += wcnss_service
 # MSM IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \
     device/qcom/msm8953_32/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
-    device/qcom/msm8953_32/msm_irqbalance_sdm632.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_sdm632.conf
+    device/qcom/msm8953_32/msm_irqbalance_little_big.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance_little_big.conf
 
 #wlan driver
 PRODUCT_COPY_FILES += \
